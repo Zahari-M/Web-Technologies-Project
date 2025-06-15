d8eb512f9fa390536d97156e24dce6a28b08276e
Zahari <zemarinov@gmail.com>
2025-06-15 19:31:22 +0300

    change databse authentication code

--- server/service/db.php
+++ server/service/db.php
@@ -1,7 +1,10 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=ASCII;charset=utf8mb4';
-$username = 'root';
-$password = '';
+$credFile = file_get_contents('../../cred.json');
+$cred = json_decode(json_decode($credFile, true)['SecretString'], true)
+$endpoint = file_get_contents("../../endpoint.txt");
+$dsn = "mysql:host=$endpoint;dbname=ASCII;charset=utf8mb4";
+$username = cred['username'];
+$password = cred['password'];
 
 try {
     $pdo = new PDO($dsn, $username, $password);
